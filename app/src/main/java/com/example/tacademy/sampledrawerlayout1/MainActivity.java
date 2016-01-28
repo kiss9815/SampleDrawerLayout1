c822c52cb3fd1e6c885393b7900eb920c4d9973e
kiss9815 <kiss9815@gmail.com>
2016-01-28 22:05:57 +0900

    toogle 사용, 백키 안먹게 하는것 complete

--- app/src/main/java/com/example/tacademy/sampledrawerlayout1/MainActivity.java
+++ app/src/main/java/com/example/tacademy/sampledrawerlayout1/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,32 +28,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-         actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_info);
+//         actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_info);
 
         drawer = (DrawerLayout)findViewById(R.id.drawer);
-        drawer.setDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
+        //
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        //토글을 만들어서 액션바에 등록하는 것, 아이콘 자동 설정, 못 바꿈
 
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_email);
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_map);
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
+//        drawer.setDrawerListener(new DrawerLayout.DrawerListener() { //drawer 상태 변할 때 바꾸는 것
+//            @Override
+//            public void onDrawerSlide(View drawerView, float slideOffset) {
+//
+//            }
+//
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_email);
+//            }
+//
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_map);
+//            }
+//
+//            @Override
+//            public void onDrawerStateChanged(int newState) {
+//
+//            }
+//        });
         menuView = (NavigationView)findViewById(R.id.navigation);
         menuView.setNavigationItemSelectedListener(this);
         if (savedInstanceState == null) {
@@ -87,14 +95,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }else if(id == android.R.id.home){
-            if(drawer.isDrawerOpen(GravityCompat.START)){
-                drawer.closeDrawer(GravityCompat.START);
-            }else {
-                drawer.openDrawer(GravityCompat.START);
-            }
-            return true;
-        }
+       }
+//         else if(id == android.R.id.home){
+//            if(drawer.isDrawerOpen(GravityCompat.START)){
+//                drawer.closeDrawer(GravityCompat.START);
+//            }else {
+//                drawer.openDrawer(GravityCompat.START);
+//            }
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -104,16 +113,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
         switch (id){
             case R.id.navi_menu_main:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, new MainFragment()).commit();
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.container, new MainFragment())
+//                        .commit();
                 break;
             case R.id.navi_menu_first:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, new FirstFragment()).commit();
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, new FirstFragment())
+                        .addToBackStack(null)
+                        .commit();
                 break;
             case R.id.navi_menu_second:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, new SecondFragment()).commit();
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, new SecondFragment())
+                        .addToBackStack(null)
+                        .commit();
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() { //drawer 가 열려있을때는 백키를 누르면 drawer를 닫히게 함
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
